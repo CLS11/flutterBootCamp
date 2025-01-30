0dbe5b5a147d3a994d8eb3a3270e04eb7cec47a1
crystall.101600 <crystall.101600@gmail.com>
2025-01-30 12:34:18 +0000

    Added navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/challenge1.dart';
+import 'package:myapp/home_page.dart';
 
 void main() { 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              'I am Rich',
-              ),
-          ),
-          backgroundColor: Colors.deepPurple,
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage(
-              'images/diamond.png',
-              ),
-            //NetworkImage(
-            //  'https://physics.aps.org/assets/58434729-f278-4bc7-ad3c-faf98d3db26b/e40_1.png'
-          //,),
-          ),
-        ),
-      ),
+      home: Heart(),
     ),
   );
 }
