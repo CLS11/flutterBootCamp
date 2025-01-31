98dae913a2f598a5ac3f61bef258c28a38dc1ab7
crystall.101600 <crystall.101600@gmail.com>
2025-01-31 09:26:24 +0000

    Amended the color scheme.

--- lib/mi_card.dart
+++ lib/mi_card.dart
@@ -7,7 +7,7 @@ class MiCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Center(
           child: Column(
@@ -23,32 +23,33 @@ class MiCard extends StatelessWidget {
               Text(
                 'Crystal Li',
                 style: GoogleFonts.lobster(
+                  color: const Color.fromARGB(255, 33, 240, 14),
                   fontSize: 40,
-                  fontWeight: FontWeight.bold,
                 ), 
               ),
               Text(
                 'ANDROID DEVELOPER',
                 style: GoogleFonts.oswald(
+                  color: const Color.fromARGB(255, 33, 240, 14),
                   fontSize: 18,
                 ),
               ),
               const Divider(
-                color: Colors.black,
+                color: Colors.white,
                 thickness: 2,
                 indent: 125,
                 endIndent: 125,
               ),
               const SizedBox(height: 20,),
-               Padding(
-                padding: const EdgeInsets.only(left: 20, right: 40,),
+               const Padding(
+                padding: EdgeInsets.only(left: 20, right: 40,),
                 child: TextField(
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
                     icon: Icon(
                       Icons.call, 
-                      color: Colors.grey[900],
+                      color:  Color.fromARGB(255, 137, 231, 30),
                       ),
                     hintText: '  + 6 5 - 5 5 5 - 7 7 7 7',
                     hintStyle: const TextStyle(
@@ -59,18 +60,18 @@ class MiCard extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 10,),
-               Padding(
-                padding: const EdgeInsets.only(left: 20, right: 40,),
+               const Padding(
+                padding: EdgeInsets.only(left: 20, right: 40,),
                 child: TextField(
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
                     icon: Icon(
                       Icons.email, 
-                      color: Colors.grey[900],
+                      color:  Color.fromARGB(255, 137, 231, 30),
                       ),
                     hintText: '  crystall.101600@gmail.com',
-                    hintStyle: const TextStyle(
+                    hintStyle:  TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
                     ),
