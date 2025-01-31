0e87ff6f83bc63ecb2a821c15cc7b20c9908fe4c
crystall.101600 <crystall.101600@gmail.com>
2025-01-31 09:02:21 +0000

    Changed the file name.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/heart.dart';
-import 'package:myapp/home_page.dart';
+// import 'package:myapp/heart.dart';
+// import 'package:myapp/home_page.dart';
+import 'package:myapp/mi_card.dart';
 
 void main() { 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Heart(),
+      home: MiCard(),
     ),
   );
 }
